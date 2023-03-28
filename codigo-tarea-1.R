ac047a02d357d04966b2d4bdd2378eca4cb89d86
Vilca <fvilca@uc.cl>
2023-03-28 00:27:18 -0600

    avance pregunta 1-2-3-4 arreglar codigo

--- codigo-tarea-1.R
+++ codigo-tarea-1.R
@@ -5,7 +5,7 @@
 #Cargar librerias
 library(tseries)
 library(lubridate)
-library(tidyverse)
+library(dplyr)
 library(lmtest) # por revisar
 library(quantmod)
 library(forecast)
@@ -79,8 +79,12 @@ varianza <- var(ts_base)
 
 boxplot(ts_base)
 
-ggplot(base, aes(GSPC.Adjusted)) +
-  geom_boxplot()
+ggplot(base, aes( x = CX.Adjusted, y = "")) +
+  geom_boxplot(fill = "#390099", color = "black",outlier.colour= "#FFBD00" ,outlier.size=2) +
+  labs(x="Valor de la acción de CEMEX",y= "",caption = "Para el périodo del 01-01-2001 al 30-06-2020")+
+  ggtitle("Dispersón de la acción de CEMEX")+
+  coord_flip()
+  theme_bw(base_size = 12)
 
 # P2
 
@@ -91,9 +95,15 @@ summary(ts_base)
 plot(ts_base)
 #abline(a = moda$locations, b= 0, add = T)
 
-base %>% 
-  ggplot(aes(GSPC.Close, )) +
-  geom_line()
+
+CX_base = data.frame(ts_base,time = seq(ISOdate(2001,01,01), 
+                                     ISOdate(2020,06,30),
+                                     by = "month"))
+
+ggplot(CX_base, aes(x = time, y = CX.Adjusted))+ #Se inicializa la gráfica
+  geom_line(color = "#390099") + #Se declara una gráfica de línea
+  labs(x = 'Tiempo', y = 'Precio de la acción', 
+       title = 'Serie de tiempo de la acción de CEMEX')
 
 
 # P4
@@ -108,13 +118,13 @@ ts <- c(ts_base, rep(0,6))
 
 ts_matrix <- matrix(ts, ncol = 12, byrow = T)
 
-pesos_b <- c(0.5,0.2,0.2,0.1)
-p.mov.pond.4 <- stats::filter(ts_base, pesos_b, sides=1)
+pesos.b1 <- c(0.5,0.2,0.2,0.1)
+p.mov.pond.4 <- stats::filter(ts_base, pesos.b1, sides=1)
 
 suma <- sum(colMeans(ts_matrix))
-pesos.b.2 <- round(colMeans(ts_matrix)/suma,4)
+pesos.b2 <- round(colMeans(ts_matrix)/suma,4)
 
-p.mov.pond.12 <- stats::filter(ts_base, pesos.b.2, sides=1)
+p.mov.pond.12 <- stats::filter(ts_base, pesos.b2, sides=1)
 
 
 # c 
@@ -135,12 +145,10 @@ ts.base.sua_099
 # e
 
 library(aTSA)
-coneldo.holt_0502 = Holt(ts_base, alpha = 0.5, beta=0.2)
-coneldo.holt_0508 = Holt(ts_base, alpha = 0.5, beta=0.8)
-
-
-coneldo.holt_0502
-coneldo.holt_0508
+ts.holt_0502 = Holt(ts_base, alpha = 0.5, beta=0.2)
+ts.holt_0508 = Holt(ts_base, alpha = 0.5, beta=0.8)
+ts.holt_0502
+ts.holt_0508
 
 # f
 
@@ -148,9 +156,6 @@ ts_base_2 <- ts(ts_matrix,start=2001, frequency = 12)
 
 hw.ts.1 = hw(ts_base, h=12,optim.start = c(0.5,0.2,0.8))     # :c
 hw.ts.2 = hw(ts_base, h=12, seasonal='additive')
-
-plot(coneldo.hw1)
-plot(coneldo.hw2)
 
 
 
